5eb45658cc3bfc4b7474f26cb8c1ade8e0c47292
Moulya <moulyagreddy@gmail.com>
2025-03-08 16:15:36 +0530

    initial commit for digital clock

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+  import {useState,useEffect} from 'react';
 function App() {
+  const [ctime,setCtime] =useState(new Date().toLocaleTimeString());
+  
+
+    useEffect(()=>{
+      const timer=setInterval(()=>{
+        setCtime(new Date().toLocaleTimeString())
+      },1000)
+
+      return ()=>clearInterval(timer);
+    },[]);
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='timer-border'>
+    <h1 className='timer'>{ctime}</h1>
     </div>
+    </>
   );
 }
 
